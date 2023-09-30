ec319c2f87320f247695d0ef687ad166ef96553f
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2023-09-30 17:09:01 +0500

    Update MyList.h

--- MyList.h
+++ MyList.h
@@ -44,7 +44,6 @@ public:
         if( m_head ) {
             Node* newHead = m_head->m_next;
             B.destroy(m_head);
-            B.deallocate(m_head, 1);
             m_head = newHead;
         }
     };

d9712e68f4b7efdc13c15a3ec52a6871ab57ac20
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2023-10-07 14:38:25 +0500

    Update memblock.h

--- memblock.h
+++ memblock.h
@@ -1,67 +1,128 @@
 #pragma once
 
-#include <memory>
-#include <vector>
+#include <cstdlib>
+#include <forward_list>
+#include <stdexcept>
 
-template<class T>
-class MemBlock{
-public:
-    MemBlock(size_t N): size(N),next(0){
-        auto p_ = std::malloc(size * sizeof(T));
-        if(!p_){
-            throw std::bad_alloc();
+template<typename T>
+class free_area
+{
+    public:
+        free_area(T* start, size_t count)
+        :m_start(start), m_count(count)
+        {}
+        bool accommodates(size_t n) const
+        {
+            return m_count >= n;
         }
-        p = reinterpret_cast<T *> (p_);
-    };
-    const size_t max_size = 10;
-    T * allocate(std::size_t n){
-		if (size + n > max_size)
-		{
-			throw std::bad_alloc();
-		}
-      	if((size_t)next < size) return (p + (int)n);
-	    else return nullptr;
-    };
-    void deallocate(T * p, std::size_t /*n_*/){
-            std::free(p);
-    };
-    bool isFull(){
-        return (size_t) next == size;
-    };
-private:
-    size_t size;// размер блока
-    T * p; //указатель на выделеную область памяти
-    int next;
+        T* occupy(size_t n)
+        {
+            auto occupied = m_start;
+            m_start += n;
+            m_count -= n;
+
+            return occupied;
+        }
+        bool is_empty() const
+        {
+            return m_count == 0;
+        }
+        bool is_after(const T* address) const
+        {
+            return address < m_start;
+        }
+        bool adjoins_left(const T* address) const
+        {
+            return address == m_start;
+        }
+        void merge_left(T* area, size_t n)
+        {
+            m_start = area;
+            m_count += n;
+        }
+        bool is_before(const T* address)
+        {
+            return m_start + m_count < address;
+        }
+        bool adjoins_right(const T* address) const
+        {
+            return m_start + m_count == address;
+        }
+        void merge_right(T* area, size_t n)
+        {
+            m_count += n;
+        }
+    private:
+        T* m_start;
+        size_t m_count;
 };
 
-template <class T>
-class MemManager{
-public:
-    MemManager(size_t n) : blockSize(n){
-  	MemBlock<T> temp(blockSize);
-        blocks.push_back(temp);
-        allocBlock = &blocks.back();
-    };
-    T * allocate(size_t n){
-        T* res = allocBlock->allocate(n);
-        if(res != nullptr) return res;
-        else {
-            for(auto i : blocks){
-                if(!i.isFull()){
-                    allocBlock = &i;
-                    return allocBlock->allocate(n);
-                }
-            }
-            blocks.push_back(MemBlock<T>(blockSize));
-            allocBlock = &blocks.back();
-            return allocBlock->allocate(n);
+template<typename T, size_t M>
+class memory_block
+{
+    public:
+        memory_block(T* memory_start)
+            :m_memory{memory_start}
+        {
+            m_free_areas.emplace_front(free_area<T>{memory_start, M});
         }
-    };
-    void deallocate(T * p, std::size_t n){
-	    allocBlock->deallocate(p, n);
-    }
-private:
-    std::vector<MemBlock<T>> blocks;
-    MemBlock<T> * allocBlock;
-    size_t blockSize;
+        T* try_get_free_memory(size_t n)
+        {
+            auto prev = m_free_areas.before_begin();
+            for(auto& current : m_free_areas)
+            {
+                if (current.accommodates(n))
+                {
+                    auto occupied = current.occupy(n);
+                    if (current.is_empty())
+                    {
+                        m_free_areas.erase_after(prev);
+                    }
+                    return occupied;
+                }
+                ++prev;
+            }
+            return nullptr;
+        }
+        bool contains_pointer(const T* area) const
+        {
+            return (area >= m_memory) && (area < m_memory + MAX_ELEMENTS);
+        }
+        void mark_as_free(T* area, size_t n)
+        {
+            if ((area < m_memory) || (area >= m_memory + MAX_ELEMENTS))
+                throw std::out_of_range("Attempt to free pointer in the wrong block");
+            auto area_end = area + n;
+            auto prev_free = m_free_areas.before_begin();
+            for (auto current_free = m_free_areas.begin(); current_free != m_free_areas.end(); ++current_free)
+            {
+                if (current_free->is_after(area_end))
+                {
+                    auto new_free_area = free_area<T>{area, n};
+                    m_free_areas.emplace_after(prev_free, new_free_area);
+                    return;
+                }
+                if (current_free->adjoins_left(area_end))
+                {
+                    current_free->merge_left(area_end, n);
+                    return;
+                }
+                if (current_free->adjoins_right(area))
+                {
+                    current_free->merge_right(area, n);
+                    return;
+                }
+                prev_free = current_free;
+            }
+            auto new_free_area = free_area<T>{area, n};
+            m_free_areas.emplace_after(prev_free, new_free_area);
+        }
+        T* get_memory()
+        {
+            return m_memory;
+        }
+    private:
+        T* m_memory;
+        const size_t MAX_ELEMENTS = M;
+        std::forward_list<free_area<T>> m_free_areas;
 };
